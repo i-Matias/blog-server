17a31faa6025fdd05c20a5d0b5e113aeb1db02aa
matias <matiasposhnjari.info@gmail.com>
2024-02-22 12:01:24 +0100

    removed unused code

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 import app from "./app";
 import router from "./route/route";
 import { config } from "./config/config";
-import connection from "./database/database";
 
 app.use("/v1", router);
-
-connection.on("connection", () => {
-  console.log("Someone is connected");
-});
 
 app.listen(config.port, () => {
   console.log(`[server]: Server is running at http://localhost:${config.port}`);
